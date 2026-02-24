b5ba835f0ca953877ab90d309f791fb4a690ba36
Unish Maharjan <uniqueunish93@gmail.com>
2026-02-24 22:44:07 +0545

    My first framework project

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,51 +3,246 @@ import React from 'react'
 export const Product = () => {
   return (
     <>
-    <div className='flex justify-center' >
-  <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md">
+   <section className="max-w-7xl mx-auto py-16 px-6">
     
-    <h2 className="text-3xl font-bold text-center mb-6">Create Account</h2>
+   
+    <div className="flex items-center gap-3 mb-4">
+      <div className="w-2 h-8 bg-black rounded"></div>
+      <p className="text-black font-semibold">Our Products</p>
+    </div>
+
     
-    <form className="space-y-4">
+
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
       
-      
-      <div>
-        <label className="block text-gray-700 mb-1">Full Name</label>
-        <input type="text" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your name"/>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Gear set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$100</span>
+          <div className="flex text-yellow-400">
+            ★★★☆☆
+          </div>
+          <span className="text-gray-500">(35)</span>
+        </div>
+      </div>
+
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Ducati hoodie</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$360</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(95)</span>
+        </div>
+      </div>
+
+     
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">ASUS FHD Gaming Laptop</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$700</span>
+          <div className="flex text-yellow-400">
+            ★★★★★
+          </div>
+          <span className="text-gray-500">(325)</span>
+        </div>
       </div>
 
       
-      <div>
-        <label className="block text-gray-700 mb-1">Email</label>
-        <input type="email" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email"/>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-xl shadow-sm hover:shadow-lg transition">
+        <img src="/public/Cart.png" className="mx-auto mb-6" />
+        <h3 className="text-lg font-medium mb-2">Rossi Tires Product Set</h3>
+        <div className="flex items-center gap-3">
+          <span className="text-red-500 font-semibold">$500</span>
+          <div className="flex text-yellow-400">
+            ★★★★☆
+          </div>
+          <span className="text-gray-500">(145)</span>
+        </div>
       </div>
 
-      
-      <div>
-        <label className="block text-gray-700 mb-1">Password</label>
-        <input type="password" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your password"/>
-      </div>
-
-      
-      <div>
-        <label className="block text-gray-700 mb-1">Confirm Password</label>
-        <input type="password" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Confirm your password"/>
-      </div>
-
-      
-      <button className="w-full bg-black text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300">
-        Sign Up
-      </button>
-
-    </form>
-
-    <p className="text-center text-gray-600 mt-4">
-      Already have an account?
-      <a href="#" className="text-blue-600 hover:underline">Login</a>
-    </p>
-
-  </div>
-  </div>
+    </div>
+  </section>
     </>
   )
 }
